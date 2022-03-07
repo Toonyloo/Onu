8618f98a29e4f76b574255617657cb464725ee1f
Toonyloo <tony0818abc@gmail.com>
2022-03-06 21:53:03 -0500

    Added special cards

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ discard = []
 for colour in ("R", "B", "G", "Y"):
     discard.insert(0, Card(colour, "0"))
     for i in range(2):
-        for value in map(str, range(1, 10)):
+        for value in list(map(str, range(1, 10))) + ["R", "D", "S"]:
             discard.append(Card(colour, value))
 
 
 def valid(card):
     colour = card.colour == discard[0].colour
     value = card.value == discard[0].value
-    return colour or value
+    return colour or value 
 
 
 def reshuffle():
@@ -62,11 +62,11 @@ def draw_game():
     top_card.reveal = True
     top_card.draw(screen)
 
-    hand_size = ((Consts.CARD_WIDTH + 10) * len(player.hand), Consts.CARD_HEIGHT + 10)
+    hand_size = ((Consts.CARD_WIDTH + 10) * len(human.hand), Consts.CARD_HEIGHT + 10)
     hand_surf = pygame.Surface(hand_size)
     hand_surf.fill((Colours.WHITE))
 
-    player.draw_hand(screen)
+    human.draw_hand(screen)
 
     txt = Fonts.title_font.render(str(len(bot1.hand)), True, Colours.WHITE)
     txt_width, txt_height = Fonts.title_font.size(str(len(bot1.hand)))
@@ -82,6 +82,12 @@ def draw_game():
     txt_width, txt_height = Fonts.title_font.size(str(len(bot3.hand)))
     txt_rect = txt.get_rect(center=(txt_width, HEIGHT / 2))
     screen.blit(txt, txt_rect)
+    
+    if turn == 0: circle_coords = (0, HEIGHT / 4)
+    if turn == 1: circle_coords = (WIDTH / 4, 0)
+    if turn == 2: circle_coords = (0, - HEIGHT / 4)
+    if turn == 3: circle_coords = (- WIDTH / 4, 0)
+    pygame.draw.circle(screen, Colours.RED, (WIDTH / 2 + circle_coords[0], HEIGHT / 2 + circle_coords[1]), 5)
 
 
 def draw_gameover():
@@ -102,19 +108,25 @@ game_state = 0
 turn = 0
 timer = 0
 winner = None
+direction = 1
 button_hovered = False
 clock = pygame.time.Clock()
+human = HumanPlayer()
+bot1 = BotPlayer()
+bot2 = BotPlayer()
+bot3 = BotPlayer()
 
 while run:
     if timer > 0:
         timer -= 1
+    clock.tick(60)
     pygame.display.update()
     screen.blit(Images.BACKGROUND, (0, 0))
-    clock.tick(60)
     keys = pygame.key.get_pressed()
     events = pygame.event.get()
     mouse_pressed = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
+    players = (human, bot1, bot2, bot3)
     for event in events:
         if event.type == pygame.QUIT:
             run = False
@@ -126,80 +138,79 @@ while run:
             button_hovered = abs(WIDTH / 2 - mouse_pos[0]) < 100 and abs(HEIGHT - 100 - mouse_pos[1]) < 50
             if mouse_pressed[0] and button_hovered:
                 reshuffle()
-                player = HumanPlayer()
+                human = HumanPlayer()
                 bot1 = BotPlayer()
                 bot2 = BotPlayer()
                 bot3 = BotPlayer()
-                for p in (player, bot1, bot2, bot3):
+                for p in (human, bot1, bot2, bot3):
                     for i in range(7):
                         p.draw_card(deck)
                 game_state = 1
                 timer = 120
 
-    if game_state == 1:
+    elif game_state == 1:
         draw_game()
         if len(deck) < 3: 
             reshuffle()
 
         if timer == 0:
             if turn == 0:
-                if True not in map(valid, player.hand):
-                    player.draw_card(deck)
+                if True not in map(valid, human.hand):
+                    human.draw_card(deck)
                     timer = 45
                 else:
                     bordered = False
-                    for i, card in enumerate(reversed(player.hand)):
+                    for i, card in enumerate(reversed(human.hand)):
                         if card.hovered(mouse_pos) and not bordered:
                             border = pygame.Rect(card.x, card.y, Consts.CARD_WIDTH, Consts.CARD_HEIGHT)
                             pygame.draw.rect(screen, Colours.BLACK, border, 3, 6)
                             bordered = True
                         if valid(card) and card.hovered(mouse_pos) and mouse_pressed[0]:
-                            player.play_card(discard, -(i + 1))
-                            turn = 1
+                            if card.value == "R":
+                                direction = -direction
+                            if card.value == "S":
+                                turn += 1 * direction
+                            if card.value == "D":
+                                turn += 1 * direction
+                                turn %= 4
+                                for _ in range(2):
+                                    players[turn].draw_card(deck)
+                            turn += 1 * direction
+                            turn %= 4
+                            human.play_card(discard, -(i + 1))
                             timer = 90
                             break
-            elif turn == 1:
-                if True not in map(valid, bot1.hand):
-                    bot1.draw_card(deck)
+            else:
+                bot = players[turn]
+                if True not in map(valid, bot.hand):
+                    bot.draw_card(deck)
                     timer = 30
                 else:
-                    for i, card in enumerate(bot1.hand):
+                    for i, card in enumerate(bot.hand):
                         if valid(card):
-                            bot1.play_card(discard, i)
-                            turn = 2
-                            timer = 90
-                            break
-            elif turn == 2:
-                if True not in map(valid, bot2.hand):
-                    bot2.draw_card(deck)
-                    timer = 30
-                else:
-                    for i, card in enumerate(bot2.hand):
-                        if valid(card):
-                            bot2.play_card(discard, i)
-                            turn = 3
-                            timer = 90
-                            break
-            elif turn == 3:
-                if True not in map(valid, bot3.hand):
-                    bot3.draw_card(deck)
-                    timer = 30
-                else:
-                    for i, card in enumerate(bot3.hand):
-                        if valid(card):
-                            bot3.play_card(discard, i)
-                            turn = 0
+                            if card.value == "R":
+                                direction = -direction
+                            if card.value == "S":
+                                turn += 1 * direction
+                            if card.value == "D":
+                                turn += 1 * direction
+                                turn %= 4
+                                for _ in range(2):
+                                    players[turn].draw_card(deck)
+                            turn += 1 * direction
+                            turn %= 4
+                            bot.play_card(discard, i)
                             timer = 90
                             break
 
-        for p in (player, bot1, bot2, bot3):
+        for p in (human, bot1, bot2, bot3):
             if len(p.hand) == 0:
                 winner = p
                 game_state = 2
                 timer = 120
 
 
-    if game_state == 2:
+    elif game_state == 2:
         button_hovered = abs(WIDTH / 2 - mouse_pos[0]) < 150 and abs(HEIGHT - 100 - mouse_pos[1]) < 50
         draw_gameover()
         
